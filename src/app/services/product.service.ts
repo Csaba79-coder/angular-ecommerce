b64b95d28b79cd4e6ad58896071f90d01d085edc
Csaba79-coder <csabavadasz79@gmail.com>
2023-02-26 10:13:23 +0100

    add service for getResponse from Product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClien: HttpClient) { 
+
+  }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClien.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
